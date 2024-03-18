05f51e3ba54ddae2f8ac7563c4493219ae2172c7
Sebastián Rojas <118761211+SebasRojasM1@users.noreply.github.com>
2024-03-18 08:25:31 -0500

    Se ha actualizado las rutas, añadiendo PATCH y DELETE

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -6,7 +6,9 @@ const NotasController = require('../controllers/notasController');
 /* ESTUDIANTES */
 /*Ruta: Llamar a todos los estudiantes (GET), e ingresar nuevos estudiantes (POST) */
 router.get('/api/estudiantes', NotasController.getAllUsers);
-router.post('/api/estudiantes/create', NotasController.createUser);
+router.post('/api/estudiantes', NotasController.createUser);
+router.patch('/api/estudiantes/update/:nombre', NotasController.updateUser);
+router.delete('/api/estudiantes/delete/:nombre', NotasController.deleteUser);
 
 
 /*Ruta: llamado del usuario por el ID */
@@ -16,12 +18,10 @@ router.get('/api/estudiantes/id/:id', NotasController.getUserByID);
 /*Ruta: llamado del usuario por el nombre */
 router.get('/api/estudiantes/nombre/:nombre', NotasController.getUserByName);
 
-
 /*Ruta: llamado del estudiante por la nota */
 router.get('/api/estudiantes/nota/:nota', NotasController.getUserByNota);
 
-
-/*Ruta: llamado de todas las notas */
+/*Ruta: llamado del estudiante por la nota */
 router.get('/api/estudiantes/notas', NotasController.getAllNotes);
 
 
